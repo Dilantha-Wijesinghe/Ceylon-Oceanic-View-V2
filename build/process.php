788afb85ac980a66d74a1e5463eb85fd7fcf1bd0
Dilantha-Wijesinghe <118370653+Dilantha-Wijesinghe@users.noreply.github.com>
2022-12-28 15:36:15 +0530

    form validation and database

--- build/process.php
+++ build/process.php
@@ -17,7 +17,7 @@
   <title>Contact Us | Ceylon Oceanic Views</title>
 </head>
 
-<body class="antialiased bg-gray-100 scrollbar-thin scrollbar-thumb-teal-500 scrollbar-track-slate-700 ">
+<body class="antialiased bg-gray-100 scrollbar-thin scrollbar-thumb-teal-500 scrollbar-track-slate-700">
 
   <header class="overflow-x-hidden ">
     <!-- bg img -->
@@ -182,6 +182,9 @@
             <ion-icon name="location" class="text-teal-300 text-xl"></ion-icon>
             <span>Pitipana, Thalagala Road, Homagama</span>
           </div>
+          <div>
+            <a href="./contact-us.html" class="inline-block self-end bg-cyan-600 text-white font-lora rounded-lg px-6 py-2 uppercase text-sm hover:bg-cyan-800 transition-colors duration-200">Return to Previous Page</a>
+          </div>
 
         </div>
 
@@ -196,6 +199,49 @@
         <!-- circles -->
         <div class="absolute z-0 w-40 h-40 bg-teal-400 rounded-full -right-28 -top-28"></div>
         <div class="absolute z-0 w-40 h-40 bg-teal-400 rounded-full -left-28 -bottom-16"></div>
+      </div>
+
+        <!-- success msg -->
+        <div id="toast-success"
+          class="flex items-center p-4 mb-4 w-full max-w-xs text-gray-600 bg-green-300 rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+          role="alert">
+          <div
+            class="inline-flex flex-shrink-0 justify-center items-center w-8 h-8 text-green-500 bg-green-400 rounded-lg dark:bg-green-800 dark:text-green-200">
+            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"></path>
+            </svg>
+            <span class="sr-only">Check icon</span>
+          </div>
+          <div class="ml-3 text-sm font-normal font-roboto">Message submitted successfully.</div>
+          <button type="button"
+            class="ml-auto -mx-1.5 -my-1.5 bg-red-300 text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+            data-dismiss-target="#toast-success" aria-label="Close">
+            <span class="sr-only">Close</span>
+            <svg aria-hidden="true" class="w-5 h-5 fill-red-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clip-rule="evenodd"></path>
+            </svg>
+          </button>
+        </div>
+
+
+
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
 
 
 <?php
@@ -228,7 +274,7 @@
 
 // use exec() because no results are returned
   $conn->exec($sql);
-  echo "Connected successfully";
+  echo "";
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
